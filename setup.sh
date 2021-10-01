c50e4cf472a0ba647a47e6be5715831f25cff01d
rajeev879 <55120534+rajeev879@users.noreply.github.com>
2021-10-01 10:27:55 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 mkdir -p ~/.streamlit/
 
+echo "\
+[general]\n\
+email = \"rajeev88771@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
-port = $PORT\n\
-enableCORS=false\n\
 headless = true\n\
-\n\
-port = $PORTn
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
